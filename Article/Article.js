364c14efc1e641cfd5f5ada707f701b724b184a1
Justin Russell <Justin@Justins-MacBook.local>
2020-02-12 13:00:41 -0800

    initial commit

--- Article/Article.js
+++ Article/Article.js
@@ -100,6 +100,7 @@ const data = [
   </div>
 
   Hint: You will need to use createElement more than once here!
+  
 
   Your function should take either an object as it's one argument, or 5 separate arguments mapping to each piece of the data object above.
 
@@ -112,3 +113,46 @@ const data = [
   Step 5: Add a new article to the array. Make sure it is in the same format as the others. Refresh the page to see the new article.
 
 */
+function newArticle(item) {
+  //create new elements
+  const article = document.createElement('div');
+  const title = document.createElement('h2');
+  const date = document.createElement('p');
+  const first = document.createElement('p');
+  const second = document.createElement('p');
+  const third = document.createElement('p');
+  const button = document.createElement('span');
+
+  //set up the structure
+  article.appendChild(title);
+  article.appendChild(date);
+  article.appendChild(first);
+  article.appendChild(second);
+  article.appendChild(third);
+  article.appendChild(button);
+
+  //add classes to elements
+  article.classList.add('article');
+  date.classList.add('date');
+  button.classList.add('expandButton');
+
+  //set text content
+  title.textContent = item.title;
+  date.textContent = item.date;
+  first.textContent = item.firstParagraph;
+  second.textContent = item.secondParagraph;
+  third.textContent = item.thirdParagraph;
+  button.textContent = 'ReadMe!';
+
+  button.addEventListener('click', () => {
+    article.classList.toggle('article-open')
+  })
+
+  return article;
+}
+
+const container = document.querySelector('.articles');
+
+data.forEach(item => {
+  container.appendChild(newArticle(item));
+});
